b33567e7a3a047c87a94a03000aad524e7bb1b7a
mohsenkamini <mohsenkamini@gmail.com>
2022-11-12 10:45:38 +0330

    scanf explained

---
+++ basics/masahat_dayere.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+ printf ("enter radius");
+ float radius;
+ scanf ("%f",&radius); // input like printf
+ float area = 3.14 * radius * radius ;
+ printf("MASAHAT= %f", area);
+ return 0;
+}
