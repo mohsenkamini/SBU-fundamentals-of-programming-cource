ba5f1f9c15bdb5117d5ccec2197e2f428ab5c42f
mohsenkamini <mohsenkamini@gmail.com>
2022-11-19 12:04:58 +0330

    bit operators

---
+++ conditions/bit-operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+
+	unsigned int a = 60;
+	unsigned int a =int b = 13;
+	int c = 0 ;
+	c = a & b ;
+	c = a | b ;
+	c = a ^ b ;	// xor
+	c = ~a ;	// one's complement
+	c = << 2 ;	// left shift zarb dar 4
+	c = >> 2 ;	// right shift taghsim bar 2
+}
