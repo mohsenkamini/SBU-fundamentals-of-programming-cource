0fb1add664b06184010f8ba623315aa637a9a670
mohsenkamini <mohsenkamini@gmail.com>
2022-12-03 11:23:14 +0330

    function overloading

---
+++ functions/function_overloading.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int func (int a, int b)
+{
+	// ...
+	//
+	// ....
+}
+
+int func (double a, double b) /// different signatures can have functions with the same names
+{
+	// ...
+	// ..
+	// ....
+}
+
+
+
