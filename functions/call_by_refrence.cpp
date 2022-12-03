ffb11e5d044fc4898e1644e92e41a27190146659
mohsenkamini <mohsenkamini@gmail.com>
2022-12-03 12:07:21 +0330

    call by reference

--- functions/call_by_refrence.cpp
+++ functions/call_by_refrence.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 // only in cpp
 // one copy of the argument is stored as a local
 // variable for the function
@@ -7,7 +7,29 @@
 void f (int & x) {//...
 }
 
+void good_swap (int & x , int & y){
+
+	int temp = x ;
+	x=y;
+	y=temp;
+}
+
+void bad_swap (int x , int y){
+
+	int temp = x ;
+	x=y;
+	y=temp;
+}
+
+
+
 int main () {
+
+	int x=3,y=4;
+	bad_swap (x,y);
+	cout << x << y;
+	good_swap (x,y);
+	cout << x << y;
 
 }
 
