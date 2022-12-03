a5b3c46c35edc6b8a1ee64467925bb294ec43f4f
mohsenkamini <mohsenkamini@gmail.com>
2022-12-03 10:56:59 +0330

    prototypes usecases

--- functions/prototypes.c
+++ functions/prototypes.c
@@ -5,6 +5,12 @@
     // V	   //
 int power (int,int);		// prototype
 				// only declare that there's power funtion
+				//
+				// in libraries we only declare functions with
+				// prototypes in .h files
+				// in definde the function in a .c file with the 
+				// same name
+				//
 int main () {
 
 	printf ("%d",power(1,2));
