dc4b063c32dc30d32f0e94f8f1a5d246d26ee9fd
mohsenkamini <mohsenkamini@gmail.com>
2022-12-13 21:52:24 +0330

    arrays as inputs are like they are called by refrence

--- arrays/passing_arrays_to_functions.c
+++ arrays/passing_arrays_to_functions.c
@@ -7,4 +7,8 @@ int main () {
 
 	int a[4];
 	f(a , 4 , a[2]);
+	// this is alike call by refrence
+	// meaning if a parameter from array a
+	// gets changed inside the function will
+	// be changes inside the array
 }
