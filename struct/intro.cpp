2a93f7b0aa16b672c0d0839bc7c9a23e76525229
mohsenkamini <mohsenkamini@gmail.com>
2023-01-04 11:32:14 +0330

    initialized struct

---
+++ struct/intro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct book 
+{
+    char* name;
+    char* author_name;
+    int publication_year;
+    double price;
+};
+
+int main () {
+    book b;
+    b.name= "shahname";
+    b.author_name= "ferdowsi";
+    b.publication_year = "1390";
+    b.price = "21.5"
+}
